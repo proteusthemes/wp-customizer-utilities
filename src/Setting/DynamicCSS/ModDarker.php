e3648a161ea06f06a82b8f6d4aebb296aca1dd40
Primoz Cigler <primoz@proteusnet.com>
2015-10-08 07:44:15 +0200

    Added inline docs.

--- src/Setting/DynamicCSS/ModDarker.php
+++ src/Setting/DynamicCSS/ModDarker.php
@@ -3,16 +3,25 @@
 namespace ProteusThemes\CustomizerUtils\Setting\DynamicCSS;
 
 /**
- * Return modified variant of the color
+ * Returns darker shade of the color.
  */
 
 class ModDarker implements ModInterface {
+
 	private $darker;
 
+	/**
+	 * @param integer $darker Darker in percents.
+	 */
 	public function __construct( $darker = 10 ) {
 		$this->darker = (int) $darker;
 	}
 
+	/**
+	 * Must-implement function from interface. Returns modified value.
+	 * @param  string $in input hex-dec color code
+	 * @return string     hex-dex darker variant
+	 */
 	public function modify( $in ) {
 		$color = new \Mexitek\PHPColors\Color( $in );
 
--- src/Setting/DynamicCSS/ModLinearGradient.php
+++ src/Setting/DynamicCSS/ModLinearGradient.php
@@ -3,17 +3,29 @@
 namespace ProteusThemes\CustomizerUtils\Setting\DynamicCSS;
 
 /**
- * Return modified variant of the color
+ * Returns linear gradient CSS property.
+ *
+ * First color is left untouched, second color is modified with the @param @modifier
  */
 
 class ModLinearGradient implements ModInterface {
+
 	private $orientation;
 
+	/**
+	 * @param ModInterface $modifier    Class which modifies the 2nd parameter.
+	 * @param string       $orientation Orientation in the string notation or in degrees (needs css unit in this case as well).
+	 */
 	public function __construct( ModInterface $modifier, $orientation = 'to bottom' ) {
 		$this->modifier    = $modifier;
 		$this->orientation = $orientation;
 	}
 
+	/**
+	 * Must-implement function from interface. Returns modified value.
+	 * @param  string $in input hex-dec color code
+	 * @return string     linear-gradient
+	 */
 	public function modify( $in ) {
 		$firstColor  = $in;
 		$secondColor = $this->modifier->modify( $in );

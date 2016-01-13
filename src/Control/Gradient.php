fdb13e64627ea43011010aa5e8818f6fbfe9f818
Primoz Cigler <primoz@proteusnet.com>
2016-01-13 15:55:57 +0100

    Update Gradient.php

--- src/Control/Gradient.php
+++ src/Control/Gradient.php
@@ -30,7 +30,7 @@ class Gradient extends \WP_Customize_Control {
 				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
 			<?php endif;
 			if ( ! empty( $this->description ) ) : ?>
-				<span class="description customize-control-description"><?php echo $this->description; ?></span>
+				<span class="description customize-control-description"><?php echo esc_html( $this->description ); ?></span>
 			<?php endif; ?>
 		</label>
 

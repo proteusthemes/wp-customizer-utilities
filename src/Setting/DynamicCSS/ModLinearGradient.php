3562737c5ea8928c0e250ddecbdba186b791e02a
Gregor Capuder <capuderg@gmail.com>
2017-06-29 09:31:03 +0200

    Add modifier for first color in linear gradient mod

--- src/Setting/DynamicCSS/ModLinearGradient.php
+++ src/Setting/DynamicCSS/ModLinearGradient.php
@@ -10,15 +10,19 @@ namespace ProteusThemes\CustomizerUtils\Setting\DynamicCSS;
 
 class ModLinearGradient implements ModInterface {
 
+	private $modifier;
 	private $orientation;
+	private $first_modifier;
 
 	/**
-	 * @param ModInterface $modifier    Instance which modifies the 2nd parameter.
-	 * @param string       $orientation Orientation in the string notation or in degrees (needs css unit in this case as well).
+	 * @param ModInterface $modifier       Instance which modifies the 2nd parameter.
+	 * @param string       $orientation    Orientation in the string notation or in degrees (needs css unit in this case as well).
+	 * @param ModInterface $first_modifier Instance which modifies the 1st parameter, which is optional.
 	 */
-	public function __construct( ModInterface $modifier, $orientation = 'to bottom' ) {
-		$this->modifier    = $modifier;
-		$this->orientation = $orientation;
+	public function __construct( ModInterface $modifier, $orientation = 'to bottom', ModInterface $first_modifier = null ) {
+		$this->modifier       = $modifier;
+		$this->orientation    = $orientation;
+		$this->first_modifier = $first_modifier;
 	}
 
 	/**
@@ -29,6 +33,10 @@ class ModLinearGradient implements ModInterface {
 	public function modify( $in ) {
 		$firstColor  = $in;
 		$secondColor = $this->modifier->modify( $in );
+
+		if ( isset( $this->first_modifier ) ) {
+			$firstColor = $this->first_modifier->modify( $in );
+		}
 
 		return sprintf( '%2$s linear-gradient(%1$s, %2$s, %3$s)', $this->orientation, $firstColor, $secondColor );
 	}
